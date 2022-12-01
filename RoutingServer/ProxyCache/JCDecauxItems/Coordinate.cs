1a91389bb42073b59096b36468683144830e5614
Benoit Gaudet <bgaudet38@gmail.com>
2022-12-01 01:13:14 +0100

    Fix missing class and try modify maxReceivedMessageSize

---
+++ RoutingServer/ProxyCache/JCDecauxItems/Coordinate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyCache.JCDecauxItems
+{
+	class Coordinate
+	{
+		public double latitude { get; set; }
+		public double longitude { get; set; }
+
+		public Coordinate(double latitude, double longitude)
+		{
+			this.latitude = latitude;
+			this.longitude = longitude;
+		}
+
+		/**
+		 * Return the latitude value with a '.' instead of a ',' to write the number in a correct format.
+		 */
+		public string GetLatitudeString()
+		{
+			return latitude.ToString().Replace(',', '.');
+		}
+
+		/**
+		 * Return the longitude value with a '.' instead of a ',' to write the number in a correct format.
+		 */
+		public string GetLongitudeString()
+		{
+			return longitude.ToString().Replace(',', '.');
+		}
+
+		public override string ToString()
+		{
+			return GetLatitudeString() + ", " + GetLongitudeString();
+		}
+	}
+}
